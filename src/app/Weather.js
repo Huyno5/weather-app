bfa574e2a7a5aa3d0f2a24b272ed81049d8eda4d
huyno5 <huyno50@gmail.com>
2021-01-19 21:35:40 -0500

    Primera version

---
+++ src/app/Weather.js
@@ -0,0 +1,24 @@
+export class Weather {
+
+	constructor(city, countryCode){
+		this.apikey='1214f0aa0f94e7e9ce310a9f7448dbae';
+		this.city= city;
+		this.countryCode= countryCode;
+	}
+
+async getWeather(){
+	const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+	const response = await fetch(URI);
+	const data = await response.json();
+	return data;
+	}
+
+
+
+	changeLocation(city, countryCode){
+		this.city = city;
+		this.countryCode = countryCode;
+
+	}
+
+}
